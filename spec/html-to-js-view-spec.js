27bdc11ee37e458d574d3730390327a77fab8e6e
Jayant Arora <jaistudy1996@gmail.com>
2017-09-05 20:45:52 -0400

    initial commit with parsing html function

---
+++ spec/html-to-js-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HtmlToJsView from '../lib/html-to-js-view';
+
+describe('HtmlToJsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
